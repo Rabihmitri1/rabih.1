1d4073366c4d1a23a77cabc9748c3d4f2618a73a
Rabih Mitri <Rabihmitri@gmail.com>
2021-12-02 15:11:01 +0200

    rabih

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,10 @@
 import { Component } from '@angular/core';
-import { Firestore, collectionData, collection } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
-
-interface Item {
-  name: string,
-  ...
-};
-
 
 @Component({
   selector: 'app-root',
-  template: `
-  <ul>
-    <li *ngFor="let item of item$ | async">
-      {{ item.name }}
-    </li>
-  </ul>
-  `
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  item$: Observable<Item[]>;
-  constructor(firestore: Firestore) {
-    const collection = collection(firestore, 'items');
-    this.item$ = collectionData(collection);
   title = 'Pixabay Image Search';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {  HttpClientModule } from '@angular/common/http';
-import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-
 
 import { AppComponent } from './app.component';
 import { ImageListComponent } from './image-list/image-list.component';
@@ -24,9 +21,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    BrowserAnimationsModule,
-    provideFirebaseApp(() => initializeApp({ ... })),
-    provideFirestore(() => getFirestore()),
+    BrowserAnimationsModule
   ],
   providers: [ImageService],
   bootstrap: [AppComponent]
